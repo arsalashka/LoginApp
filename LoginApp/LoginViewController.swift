ea79b5e279ae0edda34d8fce03ba88b71e5f5720
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-02-09 01:15:48 +0300

    ViewControllers renamed

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LoginViewController.swift
+//  ViewController.swift
 //  LoginApp
 //
 //  Created by Arsalan Balzhinimaev on 08.02.2022.
@@ -9,15 +9,60 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-	@IBOutlet var welcomeLabel: UILabel!
-	@IBOutlet var logOutButton: UIButton!
+	@IBOutlet var userNameTF: UITextField!
+	@IBOutlet var passwordTF: UITextField!
 
-	var userName: String!
+	@IBOutlet var loginButton: UIButton!
+
+	let login = "qwe"
+	let password = "123"
 
 	override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad()
+
+		// Log In Button
+		loginButton.layer.cornerRadius = 10
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+		let navigationController = segue.destination
+		navigationController.modalPresentationStyle = .fullScreen
 		
-		welcomeLabel.text = "Welcome, \(userName ?? "")!"
-		logOutButton.layer.cornerRadius = 10
-    }
+		guard let loginVC = segue.destination as? WelcomeViewController else { return }
+		loginVC.userName = userNameTF.text
+	}
+
+	@IBAction func logInButtonPressed() {
+
+		if userNameTF.text != login || passwordTF.text != password {
+			showAlert(with: "Oops!", and: "User Name or Password is wrong.\nTry again.")
+			passwordTF.text = ""
+		}
+	}
+
+	@IBAction func forgotUserNameButtonPressed() {
+		showAlert(with: "Oh my god!", and: "Your user name is \(login)")
+	}
+	
+	@IBAction func forgotPasswordButtonPressed() {
+		showAlert(with: "Really?", and: "Password is \(password)")
+	}
+
+	@IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+		userNameTF.text = ""
+		passwordTF.text = ""
+	}
+	
+}
+
+// MARK: - Private Methods
+
+extension LoginViewController {
+	private func showAlert(with title: String, and message: String) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let okAction = UIAlertAction(title: "Ok", style: .default)
+		alert.addAction(okAction)
+		present(alert, animated: true)
+	}
 }
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Arsalan Balzhinimaev on 08.02.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+	@IBOutlet var welcomeLabel: UILabel!
+	@IBOutlet var logOutButton: UIButton!
+
+	var userName: String!
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		welcomeLabel.text = "Welcome, \(userName ?? "")!"
+		logOutButton.layer.cornerRadius = 10
+    }
+}
